ff12613e8a3e0c1cdfaa511cc90f8c549c6dad22
rutkowski <waldemar.rutkowski@shopgate.com>
2017-10-12 15:19:46 +0200

    WEBC-260: Changed CartFlags class definition to fix response structure

--- extension/models/cart/CartFlags.js
+++ extension/models/cart/CartFlags.js
@@ -1,32 +1,8 @@
 class CartFlags {
-  constructor (taxIncluded, orderable, coupons) {
-    this._orderable = orderable
-    this._taxIncluded = taxIncluded
-    this._coupons = coupons
-  }
-
-  get orderable () {
-    return this._orderable
-  }
-
-  set orderable (value) {
-    this._orderable = value
-  }
-
-  get taxIncluded () {
-    return this._taxIncluded
-  }
-
-  set taxIncluded (value) {
-    this._taxIncluded = value
-  }
-
-  get coupons () {
-    return this._coupons
-  }
-
-  set coupons (value) {
-    this._coupons = value
+  constructor (orderable, taxIncluded, coupons) {
+    this.orderable = orderable
+    this.taxIncluded = taxIncluded
+    this.coupons = coupons
   }
 }
 
--- extension/models/cart/cart.js
+++ extension/models/cart/cart.js
@@ -2,15 +2,19 @@ const CartFlags = require('./CartFlags')
 
 class Cart {
   constructor () {
+    let orderable = false
+    let taxIncluded = false
+    let coupons = false
+
     this._id = null
     this._currency = null
-    this._isOrderable = false
-    this._isTaxIncluded = false
+    this._isOrderable = orderable
+    this._isTaxIncluded = taxIncluded
     this._messages = []
     this._text = null
     this._cartItems = []
     this._totals = []
-    this._flags = new CartFlags()
+    this._flags = new CartFlags(orderable, taxIncluded, coupons)
   }
 
   get id () {
