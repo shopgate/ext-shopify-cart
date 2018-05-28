e90d742ee6fa6f595c181b2b621e2b386af1dadf
Alexander Wesselburg <info@myportal.de>
2018-05-28 13:51:52 +0200

    WEBC-678 - Refactor logic

--- extension/cart/migrateCartContents_v1.js
+++ extension/cart/migrateCartContents_v1.js
@@ -1,5 +1,7 @@
 const _ = require('underscore')
 const UnknownError = require('../models/Errors/UnknownError')
+const updateCart = require('./../helper/updateCart')
+const clearCart = require('./../helper/clearCart')
 
 /**
  *
@@ -37,8 +39,6 @@ function mergeCheckoutCartItems (sourceCartLineItem, targetCartLineItem) {
 }
 
 function migrateCartContents (context, sourceCartId, sourceCartLineItems, targetCartId, targetCartLineItems, cb) {
-  const Shopify = require('../lib/shopify.api.js')(context.config)
-
   // no merge is needed on identical carts or if no items present in the source cart
   if (_.isEmpty(sourceCartLineItems) || sourceCartId === targetCartId) {
     return cb()
@@ -48,13 +48,13 @@ function migrateCartContents (context, sourceCartId, sourceCartLineItems, target
 
   // update quantity for existing items, add to the checkoutCartItems, otherwise
   _.each(sourceCartLineItems, function (sourceCartLineItem) {
-    const targetCartLineItem = _.findWhere(targetCartLineItems, {variant_id: sourceCartLineItem.variant_id})
+    const targetCartLineItem = _.findWhere(targetCartLineItems, { variant_id: sourceCartLineItem.variant_id })
     checkoutCartItems.push(mergeCheckoutCartItems(sourceCartLineItem, targetCartLineItem))
   })
 
   // re-attach the lineItems which were already in the user cart and check for duplicated items
   _.each(targetCartLineItems, function (targetCartLineItem) {
-    if (!_.findWhere(checkoutCartItems, {variant_id: targetCartLineItem.variant_id})) {
+    if (!_.findWhere(checkoutCartItems, { variant_id: targetCartLineItem.variant_id })) {
       checkoutCartItems.push(
         {
           variant_id: targetCartLineItem.variant_id,
@@ -64,61 +64,58 @@ function migrateCartContents (context, sourceCartId, sourceCartLineItems, target
     }
   })
 
+  const updatedData = { 'checkout': { 'line_items': checkoutCartItems } }
+
   // Clear the old guest cart
-  const clearData = {'checkout': {'line_items': []}}
-  Shopify.put('/admin/checkouts/' + sourceCartId + '.json', clearData, function (err) {
-    if (err) {
+  clearCart(sourceCartId, context)
+    .then(
+      updateAndAdjustCart(updatedData, targetCartId, context)
+    )
+    .catch(err => {
       context.log.error(
         'Couldn\'t clear checkout with id ' + sourceCartId + ' failed with error: ' + JSON.stringify(err)
       )
       return cb(new UnknownError())
-    }
-  })
+    })
 
-  const updatedData = {'checkout': {'line_items': checkoutCartItems}}
-
-  updateCart(updatedData, targetCartId)
-    .then(cb())
-    .catch(err => {
-      if (err.code !== 422) {
-        context.log.error(
-          'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
-        )
-        return cb(new Error('Unable to merge carts'))
-      }
-      const errorLineItem = err.error.line_items
-      Object.keys(errorLineItem).map((errorKey) => {
-        const errorContent = errorLineItem[errorKey]
-        const currentLineItem = updatedData.checkout.line_items[errorKey]
-        if (currentLineItem) {
-          const quantity = errorContent.quantity
-          if (quantity.length) {
-            quantity.map((item) => {
-              if (item.code === 'not_enough_in_stock') {
-                currentLineItem.quantity = item.options.remaining
-              }
-            })
+  /**
+   * @param {object} updatedData
+   * @param {string} targetCartId
+   * @param {object} context
+   */
+  function updateAndAdjustCart (updatedData, targetCartId, context) {
+    updateCart(updatedData, targetCartId, context)
+      .then(cb())
+      .catch(err => {
+        if (err.code !== 422) {
+          context.log.error(
+            'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
+          )
+          return cb(new Error('Unable to merge carts'))
+        }
+        const errorLineItem = err.error.line_items
+        Object.keys(errorLineItem).map((errorKey) => {
+          const errorContent = errorLineItem[errorKey]
+          const currentLineItem = updatedData.checkout.line_items[errorKey]
+          if (currentLineItem) {
+            const quantity = errorContent.quantity
+            if (quantity.length) {
+              quantity.map((item) => {
+                if (item.code === 'not_enough_in_stock') {
+                  currentLineItem.quantity = item.options.remaining
+                }
+              })
+            }
           }
-        }
-        updateCart(updatedData, targetCartId)
-          .then(cb())
-          .catch(err => {
-            context.log.error(
-              'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
-            )
-            return cb(new Error('Unable to merge carts'))
-          })
+          updateCart(updatedData, targetCartId, context)
+            .then(cb())
+            .catch(err => {
+              context.log.error(
+                'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
+              )
+              return cb(new Error('Unable to merge carts'))
+            })
+        })
       })
-    })
-
-  function updateCart (updatedData, targetCartId) {
-    return new Promise((resolve, reject) => {
-      Shopify.put('/admin/checkouts/' + targetCartId + '.json', updatedData, function (err) {
-        if (err) {
-          return reject(err)
-        }
-        resolve(true)
-      })
-    })
   }
 }
