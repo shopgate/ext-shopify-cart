bcd988521df38e4d52087f7cd7102f2c07448606
Alexander Wesselburg <info@myportal.de>
2018-05-29 15:37:47 +0200

    WEBC-678 - User can't login, if his cart contains products which are out of Stock

--- extension/cart/migrateCartContents_v1.js
+++ extension/cart/migrateCartContents_v1.js
@@ -1,7 +1,6 @@
 const _ = require('underscore')
 const UnknownError = require('../models/Errors/UnknownError')
-const updateCart = require('./../helper/updateCart')
-const clearCart = require('./../helper/clearCart')
+const { updateCart, clearCart } = require('./../helper/cart')
 
 /**
  *
@@ -65,11 +64,10 @@ function migrateCartContents (context, sourceCartId, sourceCartLineItems, target
   })
 
   const updatedData = { 'checkout': { 'line_items': checkoutCartItems } }
-
   // Clear the old guest cart
   clearCart(sourceCartId, context)
     .then(
-      updateAndAdjustCart(updatedData, targetCartId, context)
+      updateAndAdjustCart(updatedData, targetCartId, context, cb)
     )
     .catch(err => {
       context.log.error(
@@ -77,45 +75,46 @@ function migrateCartContents (context, sourceCartId, sourceCartLineItems, target
       )
       return cb(new UnknownError())
     })
+}
 
-  /**
-   * @param {object} updatedData
-   * @param {string} targetCartId
-   * @param {object} context
-   */
-  function updateAndAdjustCart (updatedData, targetCartId, context) {
-    updateCart(updatedData, targetCartId, context)
-      .then(cb())
-      .catch(err => {
-        if (err.code !== 422) {
-          context.log.error(
-            'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
-          )
-          return cb(new Error('Unable to merge carts'))
-        }
-        const errorLineItem = err.error.line_items
-        Object.keys(errorLineItem).map((errorKey) => {
-          const errorContent = errorLineItem[errorKey]
-          const currentLineItem = updatedData.checkout.line_items[errorKey]
-          if (currentLineItem) {
-            const quantity = errorContent.quantity
-            if (quantity.length) {
-              quantity.map((item) => {
-                if (item.code === 'not_enough_in_stock') {
-                  currentLineItem.quantity = item.options.remaining
-                }
-              })
-            }
-          }
-          updateCart(updatedData, targetCartId, context)
-            .then(cb())
-            .catch(err => {
-              context.log.error(
-                'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
-              )
-              return cb(new Error('Unable to merge carts'))
+/**
+ * @param {object} updatedData
+ * @param {string} targetCartId
+ * @param {object} context
+ * @param {function} cb
+ */
+function updateAndAdjustCart (updatedData, targetCartId, context, cb) {
+  updateCart(updatedData, targetCartId, context)
+    .then(cb())
+    .catch(err => {
+      if (err.code !== 422) {
+        context.log.error(
+          'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
+        )
+        return cb(new Error('Unable to merge carts'))
+      }
+      const errorLineItem = err.error.line_items
+      Object.keys(errorLineItem).map((errorKey) => {
+        const errorContent = errorLineItem[errorKey]
+        const currentLineItem = updatedData.checkout.line_items[errorKey]
+        if (currentLineItem) {
+          const quantity = errorContent.quantity
+          if (quantity.length) {
+            quantity.map((item) => {
+              if (item.code === 'not_enough_in_stock') {
+                currentLineItem.quantity = item.options.remaining
+              }
             })
-        })
+          }
+        }
+        updateCart(updatedData, targetCartId, context)
+          .then(cb())
+          .catch(err => {
+            context.log.error(
+              'Couldn\'t update checkout with id ' + targetCartId + ' failed with error: ' + JSON.stringify(err)
+            )
+            return cb(new Error('Unable to merge carts'))
+          })
       })
-  }
+    })
 }
