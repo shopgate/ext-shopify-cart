66ab3dbfb9319897f5f2cf44122e75a02b547934
Alexander Wesselburg <info@myportal.de>
2018-08-02 15:47:35 +0200

    SHOPIFY-454 - cleanup library rewrite methtod

--- extension/lib/shopgate.shopify.node.api.js
+++ extension/lib/shopgate.shopify.node.api.js
@@ -2,42 +2,40 @@ var BigJSON = require('json-bigint')
 
 module.exports = function () {
   module.makeRequest = function (endpoint, method, data, callback, retry) {
-    var https = require('https'),
-      dataString = JSON.stringify(data),
-      options = {
-        hostname: this.hostname(),
-        path: endpoint,
-        method: method.toLowerCase() || 'get',
-        port: this.port(),
-        agent: this.config.agent,
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      },
-      self = this;
+    const https = require('https')
+    const dataString = JSON.stringify(data)
+    const options = {
+      hostname: this.hostname(),
+      path: endpoint,
+      method: method.toLowerCase() || 'get',
+      port: this.port(),
+      agent: this.config.agent,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+    let self = this
 
     if (this.config.access_token) {
-      //console.log('this.config.access_token', this.config.access_token)
-      //options.headers['X-Shopify-Access-Token'] = this.config.access_token;
-      options.headers['X-Shopify-Access-Token'] = '1cd66928cfd9a78aadf644e06f1917fd';
+      options.headers['X-Shopify-Access-Token'] = this.config.access_token
     }
 
     if (options.method === 'post' || options.method === 'put' || options.method === 'delete' || options.method === 'patch') {
-      options.headers['Content-Length'] = new Buffer(dataString).length;
+      options.headers['Content-Length'] = Buffer.from(dataString).length
     }
 
-    var request = https.request(options, function (response) {
-      self.conditional_console_log('STATUS: ' + response.statusCode);
-      self.conditional_console_log('HEADERS: ' + JSON.stringify(response.headers));
+    const request = https.request(options, function (response) {
+      self.conditional_console_log('STATUS: ' + response.statusCode)
+      self.conditional_console_log('HEADERS: ' + JSON.stringify(response.headers))
 
       if (response.headers && response.headers.http_x_shopify_shop_api_call_limit) {
-        self.conditional_console_log('API_LIMIT: ' + response.headers.http_x_shopify_shop_api_call_limit);
+        self.conditional_console_log('API_LIMIT: ' + response.headers.http_x_shopify_shop_api_call_limit)
       }
 
-      response.setEncoding('utf8');
+      response.setEncoding('utf8')
 
-      var body = '';
+      let body = ''
 
       response.on('data', function (chunk) {
         self.conditional_console_log('BODY: ' + chunk)
@@ -50,63 +48,59 @@ module.exports = function () {
         // If the request is being rate limited by Shopify, try again after a delay
         if (response.statusCode === 429) {
           return setTimeout(function () {
-            self.makeRequest(endpoint, method, data, callback);
-          }, self.config.rate_limit_delay || 10000);
+            self.makeRequest(endpoint, method, data, callback)
+          }, self.config.rate_limit_delay || 10000)
         }
 
         // If the backoff limit is reached, add a delay before executing callback function
         if ((response.statusCode >= 200 || response.statusCode <= 299) && self.has_header(response, 'http_x_shopify_shop_api_call_limit')) {
-          var api_limit = parseInt(response.headers['http_x_shopify_shop_api_call_limit'].split('/')[0], 10);
-          if (api_limit >= (self.config.backoff || 35)) delay = self.config.backoff_delay || 1000; // in ms
+          const apiLimit = parseInt(response.headers['http_x_shopify_shop_api_call_limit'].split('/')[0], 10)
+          if (apiLimit >= (self.config.backoff || 35)) delay = self.config.backoff_delay || 1000
         }
 
         setTimeout(function () {
-          //console.log(response)
-          var json = {}
-            , error;
+          let json = {}
+          let error
 
           try {
-            if (body.trim() != '') { //on some requests, Shopify retuns an empty body (several spaces)
-              json = BigJSON.parse(body);
+            if (body.trim() !== '') {
+              json = BigJSON.parse(body)
             }
           } catch (e) {
-            error = e;
+            error = e
           }
           if (response.statusCode >= 400) {
             if (json && (json.hasOwnProperty('error_description') || json.hasOwnProperty('error') || json.hasOwnProperty('errors'))) {
-              var jsonError = (json.error_description || json.error || json.errors);
+              var jsonError = (json.error_description || json.error || json.errors)
             }
             error = {
-              code: response.statusCode
-              , error: jsonError || response.statusMessage
-            };
+              code: response.statusCode, error: jsonError || response.statusMessage
+            }
           }
 
-          callback(error, json, response.headers, options, response.statusCode);
-        }, delay); // Delay the callback if we reached the backoff limit
-
-      });
-
-    });
+          callback(error, json, response.headers, options, response.statusCode)
+        }, delay)
+      })
+    })
 
     request.on('error', function (e) {
-      self.conditional_console_log("Request Error: ", e);
+      self.conditional_console_log('Request Error: ', e)
       if (self.config.retry_errors && !retry) {
-        var delay = self.config.error_retry_delay || 10000;
-        self.conditional_console_log("retrying once in " + delay + " milliseconds");
+        var delay = self.config.error_retry_delay || 10000
+        self.conditional_console_log('retrying once in " + delay + " milliseconds')
         setTimeout(function () {
-          self.makeRequest(endpoint, method, data, callback, true);
-        }, delay);
+          self.makeRequest(endpoint, method, data, callback, true)
+        }, delay)
       } else {
-        callback(e);
+        callback(e)
       }
-    });
+    })
 
     if (options.method === 'post' || options.method === 'put' || options.method === 'delete' || options.method === 'patch') {
-      request.write(dataString);
+      request.write(dataString)
     }
 
-    request.end();
+    request.end()
   }
 
   return module
