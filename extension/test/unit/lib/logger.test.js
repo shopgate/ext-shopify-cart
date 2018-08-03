f1692aa8a582d56ebba1d49810c1b0e344ca70f6
Andre Kraus <andre.kraus@shopgate.com>
2018-08-03 15:19:23 +0200

    SHOPIFY-454: added unit tests for delete, post, put and get calls

--- extension/test/unit/lib/logger.test.js
+++ extension/test/unit/lib/logger.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon')
 const Logger = require('../../../lib/logger')
-const statusCodeSuccess = 200
+const httpCodeSuccess = 200
 let logRequest
 const log = {
   debug: (message) => {
@@ -14,17 +14,14 @@ describe('Logger', () => {
   });
 
   it('should log the status code, duration, request and response', () => {
-    const expectedLogObject = {
-      duration: 0,
-      message: 'Request to Shopify',
-      request: {options:{}, request:{}},
-      response: {body:{}, headers:{}},
-      statusCode: statusCodeSuccess
-    }
-
     const logSpy = sinon.spy(log, 'debug')
 
-    logRequest.log(statusCodeSuccess, {}, {}, {})
-    sinon.assert.calledWith(logSpy, expectedLogObject)
+    logRequest.log(httpCodeSuccess, {}, {}, {})
+
+    sinon.assert.calledWith(logSpy, sinon.match.has('duration'))
+    sinon.assert.calledWith(logSpy, sinon.match.has('message'))
+    sinon.assert.calledWith(logSpy, sinon.match.has('request'))
+    sinon.assert.calledWith(logSpy, sinon.match.has('response'))
+    sinon.assert.calledWith(logSpy, sinon.match({statusCode: httpCodeSuccess}))
   })
 })
