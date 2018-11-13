4b1b2d3c6374636ddc27e1ea360c0a1aa3365eec
Timo Ebel <timo.ebel@gmail.com>
2018-11-13 13:41:55 +0100

    SHOPIFY-622 Added tests for handleCartError() in cart.js helper.

--- extension/test/unit/helper/cart.test.js
+++ extension/test/unit/helper/cart.test.js
@@ -2,64 +2,57 @@ const assert = require('assert')
 
 describe('Cart Helper', () => {
   describe('extractVariantId', () => {
-    let extractVariantId
-    before(() => {
-      extractVariantId = require('../../../../extension/helper/cart').extractVariantId
-    })
+    const extractVariantId = require('../../../../extension/helper/cart').extractVariantId
 
-    it('should return null if "product" arg evaluates to false', done => {
+    it('should return null if "product" arg evaluates to false', () => {
       assert.strictEqual(extractVariantId(), null)
       assert.strictEqual(extractVariantId(undefined), null)
       assert.strictEqual(extractVariantId(null), null)
       assert.strictEqual(extractVariantId(false), null)
       assert.strictEqual(extractVariantId(0), null)
       assert.strictEqual(extractVariantId(''), null)
-      done()
     })
 
-    it('should return null if "product" arg is not an object', done => {
+    it('should return null if "product" arg is not an object', () => {
       assert.strictEqual(extractVariantId(123), null)
       assert.strictEqual(extractVariantId('123'), null)
       assert.strictEqual(extractVariantId(true), null)
       assert.strictEqual(extractVariantId([1, 2, 3]), null)
-      done()
     })
 
-    it('should return null if "product" arg has no "customData"', done => {
+    it('should return null if "product" arg has no "customData"', () => {
       assert.strictEqual(extractVariantId({}), null)
-      assert.strictEqual(extractVariantId({customData: null}), null)
-      assert.strictEqual(extractVariantId({customData: false}), null)
-      assert.strictEqual(extractVariantId({customData: ''}), null)
-      assert.strictEqual(extractVariantId({customData: 0}), null)
-      done()
+      assert.strictEqual(extractVariantId({ customData: null }), null)
+      assert.strictEqual(extractVariantId({ customData: false }), null)
+      assert.strictEqual(extractVariantId({ customData: '' }), null)
+      assert.strictEqual(extractVariantId({ customData: 0 }), null)
     })
 
-    it('should return null when "product.customData" does not contain a variant_id', done => {
-      assert.strictEqual(extractVariantId({customData: 123}), null)
-      assert.strictEqual(extractVariantId({customData: '123'}), null)
-      assert.strictEqual(extractVariantId({customData: true}), null)
-      assert.strictEqual(extractVariantId({customData: '[]'}), null)
-      assert.strictEqual(extractVariantId({customData: '[1, 2, 3]'}), null)
-      assert.strictEqual(extractVariantId({customData: '{"a": 1, "b": 2, "c": 3}'}), null)
-      assert.strictEqual(extractVariantId({customData: '{"variant_id": null}'}), null)
-      assert.strictEqual(extractVariantId({customData: '{"variant_id": false}'}), null)
-      assert.strictEqual(extractVariantId({customData: '{"variant_id": 0}'}), null)
-      assert.strictEqual(extractVariantId({customData: '{"variant_id": ""}'}), null)
-      done()
+    it('should return null when "product.customData" does not contain a variant_id', () => {
+      assert.strictEqual(extractVariantId({ customData: 123 }), null)
+      assert.strictEqual(extractVariantId({ customData: '123' }), null)
+      assert.strictEqual(extractVariantId({ customData: true }), null)
+      assert.strictEqual(extractVariantId({ customData: '[]' }), null)
+      assert.strictEqual(extractVariantId({ customData: '[1, 2, 3]' }), null)
+      assert.strictEqual(extractVariantId({ customData: '{"a": 1, "b": 2, "c": 3}' }), null)
+      assert.strictEqual(extractVariantId({ customData: '{"variant_id": null}' }), null)
+      assert.strictEqual(extractVariantId({ customData: '{"variant_id": false}' }), null)
+      assert.strictEqual(extractVariantId({ customData: '{"variant_id": 0}' }), null)
+      assert.strictEqual(extractVariantId({ customData: '{"variant_id": ""}' }), null)
     })
 
-    it('should throw a SyntaxError when "product.customData" is not valid JSON', done => {
+    it('should throw a SyntaxError when "product.customData" is not valid JSON', () => {
       const testArgs = [
         [],
         [1, 2, 3],
         {},
-        {a: 1, b: 2, c: 3},
+        { a: 1, b: 2, c: 3 },
         '{lol'
       ]
 
       testArgs.forEach(arg => {
         try {
-          extractVariantId({customData: arg})
+          extractVariantId({ customData: arg })
           assert.fail('Expected a SyntaxError to be thrown; none thrown.')
         } catch (err) {
           if (!(err instanceof SyntaxError)) {
@@ -68,13 +61,91 @@ describe('Cart Helper', () => {
           }
         }
       })
-
-      done()
     })
 
-    it('should return a variant ID if "product.customData" does include one', done => {
+    it('should return a variant ID if "product.customData" does include one', () => {
       assert.strictEqual(extractVariantId({customData: '{"variant_id": "123"}'}), '123')
-      done()
+    })
+  })
+
+  describe('handleCartError', () => {
+    const handleCartError = require('../../../../extension/helper/cart').handleCartError
+
+    it('should throw the error if it is not in the expected format', () => {
+      const testArgs = [
+        new Error('not what we expected'),
+        { errors: { message: 'still not what we expected' } },
+        { errors: { line_items: false } }
+      ]
+
+      testArgs.forEach(arg => {
+        try {
+          handleCartError(arg)
+          assert.fail('Expected an error to be thrown; none thrown.')
+        } catch (err) {
+          // all fine
+        }
+      })
+    })
+
+    it('should return a "flattened" summary of all error messages', () => {
+      const error = new Error()
+      error.errors = {
+        line_items: {
+          0: {
+            quantity: [
+              { code: 'some_error_code', message: 'some message' },
+              { code: 'some_other_error_code', message: 'some other message' }
+            ],
+            stuff: [
+              { code: 'stuff_error_code', message: 'stuff message' },
+              { code: 'stuff_other_error_code', message: 'stuff other message' }
+            ]
+          },
+          4: {
+            lol: [
+              { code: 'lol_code', message: 'lol' },
+              { code: 'rofl_code', message: 'rofl' }
+            ],
+            rofl: [
+              { code: 'lolz_code', message: 'lolz' },
+              { code: 'roflmao_code', message: 'roflmao' }
+            ]
+          }
+        }
+      }
+
+      assert.deepStrictEqual(
+        handleCartError(error),
+        [
+          { code: 'some_error_code', message: 'some message', type: 'error' },
+          { code: 'some_other_error_code', message: 'some other message', type: 'error' },
+          { code: 'stuff_error_code', message: 'stuff message', type: 'error' },
+          { code: 'stuff_other_error_code', message: 'stuff other message', type: 'error' },
+          { code: 'lol_code', message: 'lol', type: 'error' },
+          { code: 'rofl_code', message: 'rofl', type: 'error' },
+          { code: 'lolz_code', message: 'lolz', type: 'error' },
+          { code: 'roflmao_code', message: 'roflmao', type: 'error' }
+        ]
+      )
+    })
+
+    it('should convert "not_enough_in_stock" error codes into "EINSUFFICIENTSTOCK"', () => {
+      const error = new Error()
+      error.errors = {
+        line_items: {
+          0: {
+            quantity: [
+              { code: 'not_enough_in_stock', message: 'some message' }
+            ]
+          }
+        }
+      }
+
+      assert.deepStrictEqual(
+        handleCartError(error),
+        [{ code: 'EINSUFFICIENTSTOCK', message: 'some message', type: 'error' }]
+      )
     })
   })
 })
