96b4faeef9f34ea3eb322b64f5570a25ae9f7a0d
Timo Ebel <timo.ebel@gmail.com>
2018-11-14 14:24:20 +0100

    SHOPIFY-622 Made the initialization script for newman environment a bit more generic.

--- extension/test/newman/setup.js
+++ extension/test/newman/setup.js
@@ -1,20 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 
-if (!process.argv[2]) {
-  console.log('Usage: setupNewman [product_id_limited_stock_of_2]\n')
+const envTemplate = JSON.parse(fs.readFileSync(path.join(__dirname, 'environment-template.json')))
+
+const settableValues = envTemplate.values.filter(value => value.value === '***')
+
+if ((process.argv.length - 2) !== settableValues.length) {
+  console.log('Usage:')
+  console.log(`setupNewman [${settableValues.map(value => value.key).join('] [')}]\n`)
   console.log('Parameters:')
-  console.log('product_id_limited_stock_of_2      A Shopify product ID of a product with stock limited to 2 and active inventory control.\n')
+  settableValues.forEach(value => console.log(`${value.key}\t${value.description}`))
+  console.log()
   process.exit(0)
 }
 
-const envTemplate = JSON.parse(fs.readFileSync(path.join(__dirname, 'environment-template.json')))
-
-envTemplate.values = envTemplate.values.map(value => {
-  const newValue = Object.assign({}, value)
-  if (newValue.key === 'product_id_limited_stock_of_2') newValue.value = process.argv[2]
-
-  return newValue
+settableValues.forEach((value, index) => {
+  envTemplate.values.find(originalValue => originalValue.key === value.key).value = process.argv[index + 2]
 })
 
 fs.writeFileSync(path.join(__dirname, 'environment.json'), JSON.stringify(envTemplate, null, 2))
