2d4fc86c077c69e4f403c1366a03d68274df87c0
SG-Noxoreos <waldemar.rutkowski+github@shopgate.com>
2018-12-06 15:12:30 +0100

    PWA-1210: Improved implementation. Fixed some linter errors.

--- extension/cart/deleteProductsInput_v1.js
+++
@@ -1,9 +0,0 @@
-/**
- * Map CartItem to cartItem to comply to new schema
- * @param {Object} context context
- * @param {{CartItemIds: Object[], cartItemIds: Object[]}}input input
- * @returns {Promise<{cartItemIds: Object[]}>}
- */
-module.exports = async (context, { CartItemIds, cartItemIds: inputCartItemIds }) => ({
-  cartItemIds: inputCartItemIds || CartItemIds
-})
--- extension/cart/updateProductsInput_v1.js
+++
@@ -1,9 +0,0 @@
-/**
- * Map CartItem to cartItem to comply to new schema
- * @param {Object} context context
- * @param {{CartItem: Object[], cartItems: Object[]}} input input
- * @returns {Promise<{cartItems: Object[]}>}
- */
-module.exports = async (context, { CartItem, cartItems: inputCartItems }) => ({
-  cartItems: inputCartItems || CartItem
-})
