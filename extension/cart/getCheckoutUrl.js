f8f83b1226da84232fafcf50ae1519530f3ce867
Timo Ebel <timo.ebel@shopgate.com>
2024-10-02 18:10:53 +0200

    CURB-3804 Changed the way the checkout URL is retrieved to use the one attached to a cart from the Cart API.

--- extension/cart/getCheckoutUrl.js
+++ extension/cart/getCheckoutUrl.js
@@ -1,27 +1,8 @@
-const Url = require('../models/url')
-const ConfigHelper = require('../helper/config')
-
-/**
- * @typedef {Object} context
- * @property {Object} config
- *
- * @typedef {Object} config
- * @property {String} shopifyShopAlias
- * @property {String} shopifyShopDomain
- */
 /**
  *
- * @param {Object} context
- * @param {Object} input
+ * @param {SDKContext} context
+ * @param {{ shopifyCart: ShopifyCart }} input
  */
 module.exports = async (context, input) => {
-  // limit expiry time to two hours from "now", because we don't have any info about the real expiry time
-  const date = new Date()
-  date.setTime(date.getTime() + 1000 * 60 * 60 * 2)
-  const expires = date.toISOString()
-
-  // We need to replace the web_url from data.checkout with the shopify shop domain (using the alias from our config)
-  const checkoutDomain = input.checkout.web_url.replace(context.config.shopifyShopAlias + '.myshopify.com', ConfigHelper.getHostName(context.config))
-
-  return new Url(checkoutDomain, expires)
+  return { url: input.shopifyCart.checkoutUrl }
 }
