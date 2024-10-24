b4901749186361d5fd5f80f548d6d257c9c09cf3
Timo Ebel <timo.ebel@shopgate.com>
2024-10-24 15:24:37 +0200

    CURB-3804 More error handling.

--- extension/cart/fetchShopifyCart.js
+++ extension/cart/fetchShopifyCart.js
@@ -9,11 +9,25 @@ const UnknownError = require('../models/Errors/UnknownError')
 module.exports = async (context, { shopifyCartId }) => {
   const storefrontApi = ApiFactory.buildStorefrontApi(context)
 
-  const shopifyCart = await storefrontApi.getCart(shopifyCartId)
+  let shopifyCart
+  try {
+    shopifyCart = await storefrontApi.getCart(shopifyCartId)
+  } catch (err) {
+    this.log.error({ errorMessage: err.message, statusCode: err.statusCode, code: err.code }, 'Error fetching shopify cart')
+    throw new UnknownError()
+  }
 
   if (shopifyCart === null) {
-    context.log.error('Shopify API returned null instead of a cart.')
-    throw new UnknownError()
+    context.log.warn('Shopify API returned null when getting the device cart, creating a new one')
+    try {
+      const newDeviceCartId = await storefrontApi.createCart()
+      shopifyCart = await storefrontApi.getCart(newDeviceCartId)
+      await context.storage.device.set('shopifyCartId', newDeviceCartId)
+    } catch (err) {
+      await context.storage.device.del('shopifyCartId')
+      this.log.error({ errorMessage: err.message, statusCode: err.statusCode, code: err.code }, 'Error creating new device cart')
+      throw new UnknownError()
+    }
   }
 
   return { shopifyCart }
