8e8ec2d3ebe2a7294e5779c41468d6885a212d42
Timo Ebel <timo.ebel@shopgate.com>
2024-12-19 19:42:34 +0100

    Fix users remaining logged out in the checkout until checkout completion when cart buyer identity expired or they once actively logged out on the checkout page.

--- extension/cart/getCheckoutUrl.js
+++ extension/cart/getCheckoutUrl.js
@@ -1,8 +1,27 @@
+const ApiFactory = require('../lib/ShopifyApiFactory')
+
 /**
  * @param {SDKContext} context
- * @param {{ shopifyCart: ShopifyCart }} input
+ * @param {{ shopifyCartId: string, shopifyCart: ShopifyCart, storefrontApiCustomerAccessToken: StorefrontApiCustomerAccessToken, sgxsMeta: SgxsMeta }} input
  */
 module.exports = async (context, input) => {
+  // It seems like on some shops the cart buyer identity gets cleared after a while. If user is logged in but no buyer
+  // identity mapped to the cart, update it with their access token so the user will be logged in when entering the
+  // checkout.
+  if (context.meta.userId && !(input.shopifyCart.buyerIdentity || {}).customer) {
+    context.log.debug('User is logged in but cart has no buyer identity, updating cart...')
+    const storefrontApi = ApiFactory.buildStorefrontApi(context, input.sgxsMeta)
+
+    try {
+      await storefrontApi.updateCartBuyerIdentity(input.shopifyCartId, input.storefrontApiCustomerAccessToken)
+
+      // new cart with new checkout URL must be fetched for the changes to reflect in the checkout
+      input.shopifyCart = await storefrontApi.getCart(input.shopifyCartId)
+    } catch (err) {
+      context.log.error({ errorMessage: err.message, statusCode: err.statusCode, code: err.code }, 'Error updating buyer identity in the cart')
+    }
+  }
+
   // Once we left for the checkout there's no way of knowing whether the checkout has been completed (invalidating the
   // cart ID) or aborted (keeping the cart ID valid). Setting a flag that will cause the initShopifyCart step to fetch
   // the cart from Shopify Cart API to see if it is still valid.
